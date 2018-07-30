9375a891a8b8cad94f68beace0b69193ce6ecdfd
Maayan Kfir <mayankfir@gmail.com>
2018-07-30 20:06:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,13 @@ def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
 
+def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move(index)
+    move(index)
+    display_board
+  end
+  
 end
